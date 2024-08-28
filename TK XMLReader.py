4e4bc400ba62b3c454b97d0625e6250802f25d97
Victor Carbón Ferreira <92864869+Vcarbon25@users.noreply.github.com>
2024-08-28 15:39:22 -0300

    Add files via upload

--- TK XMLReader.py
+++ TK XMLReader.py
@@ -2,12 +2,17 @@
 import tkinter  as TK
 from tkinter import filedialog
 from lxml import etree
+
+
 root = TK.Tk()
-
-
+#set window size
 root.geometry("700x350")
-
-
+#set screens top menu
+up = root.winfo_toplevel() #get screen's top border location
+root.menuBar = TK.Menu(up)
+up["menu"] = root.menuBar
+root.subMenu = TK.Menu(root.menuBar)
+root.menuBar.add_cascade(label="File", menu=root.subMenu)
 #Add a Vertical Scrollbar
 scroll_v = TK.Scrollbar(root)
 scroll_v.pack(side= TK.RIGHT,fill="y")
@@ -16,8 +21,7 @@ scroll_v.pack(side= TK.RIGHT,fill="y")
 scroll_h = TK.Scrollbar(root, orient= TK.HORIZONTAL)
 scroll_h.pack(side= TK.BOTTOM, fill= "x")
 
-BtOpen=TK.Button(root,text="Open XML")
-BtOpen.pack()
+
 #Add a Text widget
 text = TK.Text(root, height= 500, width= 350, yscrollcommand= scroll_v.set,
 xscrollcommand = scroll_h.set, wrap= TK.NONE, font= ('Helvetica 15'))
@@ -25,13 +29,12 @@ text.pack(fill = TK.BOTH, expand=0)
 
 def OpenFile():
     arq=filedialog.askopenfilename(initialdir="Documents")
-    parser=etree.XMLParser(recover=True,encoding="utf-8")
+    parser=etree.XMLParser(recover=True,encoding="utf-8", remove_blank_text=True)
     XMLFile = etree.parse(arq,parser=parser)
     
     text.delete('1.0',TK.END)
     text.insert('1.0', etree.tostring(XMLFile, pretty_print=True,encoding=str))
-BtOpen.config(command=OpenFile)
-
+root.subMenu.add_command(label="Open",command=OpenFile)
 #Attact the scrollbar with the text widget
 scroll_h.config(command = text.xview)
 scroll_v.config(command = text.yview)
