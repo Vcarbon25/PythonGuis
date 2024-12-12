1faf1d94b634830cbac2d79626b0ab91eedaec1e
Victor Carbón Ferreira <92864869+Vcarbon25@users.noreply.github.com>
2024-12-12 14:48:03 -0300

    Add files via upload

--- TK_ResponsiveWindow.py
+++ TK_ResponsiveWindow.py
@@ -1,13 +1,20 @@
 import tkinter as TK
 
-root = TK.Tk()
+window = TK.Tk()
+root = TK.LabelFrame(window,text="application")
+root.pack()
+
 BtAdd = TK.Button(root,text="Add row:")
 BtAdd.grid(row=0,column=0)
-LFContainer = TK.LabelFrame(root,text="content",height=300,width=250)
-LFContainer.grid(row=0,column=1)
+CvContainer = TK.Canvas(root,height=300,width=250)
+CvContainer.grid(row=0,column=1)
+YScroll = TK.Scrollbar(CvContainer,orient='vertical')
+YScroll.pack(side=TK.RIGHT,fill="y")
+CvContainer.config(yscrollcommand=YScroll.set)
+YScroll.config(command=CvContainer.yview)
 
 def AddRow():
-    LFAtr=TK.LabelFrame(LFContainer,text="atr",width=70,height=50,padx=20,pady=10)
+    LFAtr=TK.LabelFrame(CvContainer,text="atr",width=70,height=50,padx=20,pady=10)
     LFAtr.pack()
     EnInfo = TK.Entry(LFAtr)
     EnInfo.grid(row=0,column=0)
