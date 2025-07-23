5846a3e21e896fbc7adf611a5de283c5ce52d277
Victor Carbón Ferreira <92864869+Vcarbon25@users.noreply.github.com>
2025-07-23 07:53:16 -0300

    Add files via upload

---
+++ TK_Database_Workbench.py
@@ -0,0 +1,94 @@
+import tkinter as TK
+import tkinter.filedialog as FD
+import sqlite3
+
+class App():
+    def __init__(self,master=None):
+        master.title("SQL Workbench")
+        self.LFControls = TK.LabelFrame(master,text="Controls")
+        self.LFControls.grid(column=0,row=0,rowspan=5)
+        self.LbFile=TK.Label(self.LFControls,text="Select File")
+        self.LbFile.grid(row=0,column=0)
+        self.BtNewFile = TK.Button(self.LFControls,text="New Database",command=self.New_DB)
+        self.BtNewFile.grid(row=1,column=0)
+        self.BtFile = TK.Button(self.LFControls,text="Choose File",command=self.Open_File)
+        self.BtFile.grid(row=2,column=0)
+        
+        self.BtSave = TK.Button(self.LFControls,text="Save Changes",command=self.Save)
+        self.BtSave.grid(row=3,column=0)
+        self.BtHelp = TK.Button(self.LFControls,text="SQL help",command=self.SQLHelp)
+        self.BtHelp.grid(row=4,column=0)
+        self.TxInteractions = TK.Text(master,border=2)
+        self.TxInteractions.grid(row=1,column=1,columnspan=3)
+        self.LbIn = TK.Label(master,text="Input: ")
+        self.LbIn.grid(row=2,column=1)
+        self.TxIn = TK.Entry(master,width=60)
+        self.TxIn.grid(row=2,column=2)
+        self.File=''
+        self.BtOk=TK.Button(master,text="Send Command",command=self.Update_Interaction)
+        self.BtOk.grid(row=2,column=0)
+
+    def Update_Interaction(self,*text):
+        UserIn=self.TxIn.get()
+        self.TxIn.delete(0,TK.END)
+        if self.File=='':
+            self.TxInteractions.insert(TK.END,"Select File\n\n")
+        if UserIn.strip()!='':
+            self.TxInteractions.insert(TK.END, UserIn+"\n")
+            self.TxIn.delete(0,TK.END)
+            try: #will execute this block in select statements
+                self.cursor.execute(UserIn)
+                query = self.cursor.fetchall()
+                for row in query:
+
+                    self.TxInteractions.insert(TK.END,str(row)+"\n")
+            except:
+                pass
+        if text!="":
+            self.TxInteractions.insert(TK.END, str(text)+"\n")
+            
+            
+    def Open_File(self):
+        self.File = FD.askopenfilename(initialdir="Desktop")
+        self.LbFile.config(text=self.File.split("/")[-1])
+        if self.File.split(".")[-1]=="db":
+            self.Db=sqlite3.connect(self.File)
+            self.cursor=self.Db.cursor()
+            self.cursor.execute('SELECT name FROM sqlite_master WHERE type="table";')
+            self.TxIn.delete(0,TK.END)
+            self.Update_Interaction("Tabelas Disponiveis: "+str(self.cursor.fetchall())+"\n")
+    def New_DB(self):
+        self.File = FD.asksaveasfilename(initialdir="Desktop",filetypes=(("Databases","*.db"),))
+       
+        self.LbFile.config(text=self.File.split("/")[-1])
+        if self.File.split(".")[-1]=="db":
+            self.Db=sqlite3.connect(self.File)
+            self.cursor=self.Db.cursor()
+            self.Update_Interaction("Database Created")
+    def Save(self):
+        self.Db.commit()
+        self.Update_Interaction("Commit")
+    def SQLHelp(self):
+        self.HelpWindow=TK.Toplevel()
+        self.HelpWindow.title("SQL tips")
+        self.LbReference=TK.Label(self.HelpWindow,text="Reference:  https://www.sqlite.org/")
+        self.LbHelp1 = TK.Label(self.HelpWindow,text=""">>CREATE NEW TABLE\n CREATE TABLE name (Colunmname type....)
+                                types can be:  text, numeric, interger, real, blob""")
+        self.LbHelp1.grid(row=0,column=0)
+        self.LbHelp2=TK.Label(self.HelpWindow,text=""">>GET TABLE DETAILS\n PRAGMA table_info(table_name)""")
+        
+        self.LbHelp2.grid(row=1,column=0)
+        self.LbHelp3=TK.Label(self.HelpWindow,text=">>UPDATE TABLE COLUNMS\n ALTER TABLE tableName ADD colunmname type\n ALTER TABLE tablename DROP ColunmName")
+        self.LbHelp3.grid(row=2,column=0)
+        self.LbHelp4=TK.Label(self.HelpWindow,text=">>DELETE TABLE\n DROP TABLE tableName")
+        self.LbHelp4.grid(row=3,column=0)
+        self.LBHelp5=TK.Label(self.HelpWindow,text=""">>INSERT ROW IN TABLE\n INSERT INTO tablename VALUES(value1, value2....)""")
+        self.LBHelp5.grid(row=4,column=0)
+        self.LbHelp6 = TK.Label(self.HelpWindow,text=">> UPDATE ROW\n UPDATE nometabela SET colunmName=Value\n WHERE colunm condition")
+        self.LbHelp6.grid(row=5,column=0)
+
+        self.LBHelp6 = TK.Label(self.HelpWindow,text=""">>SELECT DATA FROM TABLE\nSELECT * (for all colunms\n or ) colunm names\n FROM tablename\n WHERE colum condition""")
+        self.LBHelp6.grid(row=5,column=0)
+root=TK.Tk()
+App(root)
+root.mainloop()
